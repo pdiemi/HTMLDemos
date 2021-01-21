825f03def3686dddb8df6886b36f548d40ed8280
pdiemi <phamdiem0902@gmail.com>
2021-01-21 12:04:02 -0600

    updated

---
+++ HTMLDEMOS/21Jan2021/js/objects.js
@@ -0,0 +1,5 @@
+// {}
+
+var students={id:101, name:"admin"}
+
+console.log(students.id + "\t" +students.name)
--- HTMLDEMOS/21Jan2021/js/welcome.js
+++ HTMLDEMOS/21Jan2021/js/welcome.js
@@ -13,5 +13,27 @@ function showdata() {
 function showdata2(){
     alert(mydata)
 }
-showdata();
-showdata2();
+//showdata();
+//showdata2();
+
+function collectdata(){
+    var nameData=document.getElementById("uname").value
+    var emailData=document.getElementById("uemail").value
+    console.log(nameData + "\n" + emailData)
+}
+//Calculator
+function add(){
+    var x=document.getElementById("x").value
+    var y=document.getElementById("y").value
+    console.log("x + y = "+ (Number (x) + Number (y)))
+}
+function sub(){
+    var x=document.getElementById("x").value
+    var y=document.getElementById("y").value
+    console.log("x - y = "+ (Number (x) - Number (y)))
+}
+function mul(){
+    var x=document.getElementById("x").value
+    var y=document.getElementById("y").value
+    console.log("x * y = "+ (Number (x) * Number (y)))
+}
