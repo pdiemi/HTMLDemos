947c86ad7a5c0916f6dd67339e24eaf1334868c8
pdiemi <phamdiem0902@gmail.com>
2021-01-21 11:01:08 -0600

    updated

---
+++ HTMLDEMOS/21Jan2021/js/loop.js
@@ -0,0 +1,16 @@
+//for loop
+function forDemo(){
+document.write("For loop <br>")
+    for(var i=0; i<=10; i++){
+        document.write(i, "<br>")
+    }
+}
+//while loop
+function whileDemo(){
+    document.write("While loop <br>")
+    var a=11
+    while(a<20){
+        document.write(a,"<br>")
+        a++
+    }
+}
--- HTMLDEMOS/21Jan2021/js/welcome.js
+++ HTMLDEMOS/21Jan2021/js/welcome.js
@@ -4,9 +4,11 @@ var data=document.getElementById("show")
 data.innerHTML="new data"
 
 var mydata=10;
+mydata="hello!"
 
 function showdata() {
-    alert(mydata)
+    var newdata=70
+    alert(newdata)
 }
 function showdata2(){
     alert(mydata)
